32dd4c2f297a8a4ea03643e210d4ca8497b7a823
Nerando Johnson <nerajno@gmail.com>
2023-07-01 23:54:32 -0400

    Updated the Editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { store } from '../store/reducers';
+// import { store } from '../store/reducers';
 
 const Editor = ({disDispatch}) => {
-    const [text, setText] = useState(store.getState().text);
+     const dispatch = useDispatch();
+    const [text, setText] = useState('');
 
     const handleChange = (e) => {
-        setText(e.target.value);
-        disDispatch({type: 'UPDATE_TEXT', 
-        text: e.target.value});
+        const newText = e.target.value;
+        setText(newText);
+        dispatch({type: 'UPDATE_TEXT', payload: newText});
     }
-    
+
     return (
         <div id='editorComponent'>
         <h2>Editor</h2>
