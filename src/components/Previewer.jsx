7425af92b4d0f2d1cfff5923f2613f79bbc5ad35
Nerando Johnson <nerajno@gmail.com>
2023-07-03 23:11:24 -0400

    Added Markdown

--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectText } from '../store/text/selectors';
+import { marked } from 'marked';
 
 const Previewer = () => {
     const text = useSelector(selectText);
     return (
         <div id='preview'>
             <h2>Previewer</h2>
-            <p>{text}</p>
+            <p>{marked.parse(text)}</p>
         </div>
     );
 };
